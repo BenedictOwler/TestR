8003aba3aee76931edeb1d543fcf5ee4d4d452a1
BenedictOwler <43406176+BenedictOwler@users.noreply.github.com>
2018-09-25 13:47:23 +0200

    Update My_firstname_my_lastname.c

--- My_firstname_my_lastname.c
+++ My_firstname_my_lastname.c
@@ -1,5 +1,12 @@
 int main () {
 char * vorname = "Benedict";
 char * nachname = "Owler";
+char * schule = "Bundeschule";
+char * klasse = "2ABCD" ;
+
 
 printf("Hallo, mein Name ist %s %s.\n" ,vorname, nachname);
+
+printf("Ich gehe in die %s als auch die Klasse %s.
+       
+       }
